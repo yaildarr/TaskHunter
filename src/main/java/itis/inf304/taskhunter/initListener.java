ad741db5d803d7da03c7781dd11b92104f4559de
yaildarr <ildartanki@mail.ru>
2024-09-27 20:49:08 +0300

    <>

---
+++ src/main/java/itis/inf304/taskhunter/initListener.java
@@ -0,0 +1,21 @@
+package itis.inf304.taskhunter;
+
+import itis.inf304.taskhunter.dao.UserDao;
+import itis.inf304.taskhunter.service.SecurityService;
+import itis.inf304.taskhunter.util.ConnectionProvider;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class initListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try{
+            ConnectionProvider connectionProvider = ConnectionProvider.getInstance();
+            sce.getServletContext().setAttribute("userDao", new UserDao(connectionProvider));
+            sce.getServletContext().setAttribute("securityService", new SecurityService());
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка иницализации" , e);
+        }
+    }
+}
