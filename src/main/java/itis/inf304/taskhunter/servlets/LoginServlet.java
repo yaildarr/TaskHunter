5971df030a95b8947c5b9d2d46403db022698fdb
yaildarr <ildartanki@mail.ru>
2024-09-27 21:11:08 +0300

    login

--- src/main/java/itis/inf304/taskhunter/servlets/LoginServlet.java
+++ src/main/java/itis/inf304/taskhunter/servlets/LoginServlet.java
@@ -5,17 +5,26 @@ import itis.inf304.taskhunter.entities.User;
 import itis.inf304.taskhunter.service.SecurityService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     SecurityService securityService = (SecurityService) getServletContext().getAttribute("securityService");
     UserDao userDao = (UserDao) getServletContext().getAttribute("userDao");
     User user;
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/view/login.jsp").forward(req, resp);
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
