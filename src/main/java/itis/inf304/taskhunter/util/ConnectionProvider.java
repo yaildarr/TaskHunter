85a77ddd205b358c94e0b61cb75b73ead74bbb4b
yaildarr <ildartanki@mail.ru>
2024-09-27 18:29:47 +0300

    add dao, some jsp, entities, SecurityFilter, SecurityService

---
+++ src/main/java/itis/inf304/taskhunter/util/ConnectionProvider.java
@@ -0,0 +1,30 @@
+package itis.inf304.taskhunter.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+    private static ConnectionProvider _instance;
+
+    public static ConnectionProvider getInstance() {
+        if (_instance == null) {
+            _instance = new ConnectionProvider();
+        }
+        return _instance;
+    }
+    private Connection connection;
+
+    private ConnectionProvider(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/taskhunter", "root", "546741qwerty");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
--- src/main/webapp/WEB-INF/view/categories.jsp
+++ src/main/webapp/WEB-INF/view/categories.jsp
@@ -1,7 +1,6 @@
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@include file="/WEB-INF/view/parts/header.jsp" %>
 
-<h1>Категории</h1>
+<h1>Главная</h1>
 
 <%@include file="/WEB-INF/view/parts/footer.jsp" %>
